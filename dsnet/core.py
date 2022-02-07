bfdcadc5c6abd18f5e943b041004d5d9c0321046
Bruno Thomas <bthomas@icij.org>
2022-02-07 18:11:55 +0000

    fix: add init parameter for peer_key from db
    Co-authored-by: Laurent Girod <laurent-girod@users.noreply.github.com>

--- dsnet/core.py
+++ dsnet/core.py
@@ -15,10 +15,10 @@ PH_MESSAGE_LENGTH: int = 2048
 
 class PigeonHole:
     def __init__(self, public_key_for_dh: bytes, private_key_for_dh: bytes = None, sender_public_key: bytes = None,
-                 message_number: int = 0, dh_key: bytes = None, conversation_id: Optional[int] = None) -> None:
+                 message_number: int = 0, dh_key: bytes = None, peer_key: bytes = None, conversation_id: Optional[int] = None) -> None:
         self.dh_key = compute_dhke(private_key_for_dh, public_key_for_dh) if dh_key is None else dh_key
         self.public_key = sender_public_key if sender_public_key is not None else public_key_for_dh
-        self.peer_key = public_key_for_dh if sender_public_key is not None else None
+        self.peer_key = peer_key if peer_key is not None else public_key_for_dh if sender_public_key is not None else None
         self.message_number = message_number
         self.conversation_id = conversation_id
 
